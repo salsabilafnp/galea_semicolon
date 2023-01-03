5e5ac913f13b89f462c0e7f15cff108d772d79ff
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-01-03 23:21:03 +0700

    create product

--- app/Http/Controllers/DashboardProductsController.php
+++ app/Http/Controllers/DashboardProductsController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class DashboardProductsController extends Controller
 {
@@ -28,7 +31,9 @@ class DashboardProductsController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.products.create', [
+            "title" => "Produk Galea"
+        ]);
     }
 
     /**
@@ -39,7 +44,35 @@ class DashboardProductsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validate form
+        $validatedData = $request->validate([
+            'nama_produk'           => 'required',
+            'harga'                 => 'required',
+            'banyak_produk'         => 'required',
+            'kategori'              => 'required',
+            'deskripsi'             => 'required',
+            'gambar'                => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        $validatedData['slug'] = Str::slug($request->nama_produk, '-');
+
+        //upload image
+        $gambar = $request->file('gambar');
+        $gambar->store('products', $gambar->hashName());
+
+        //create post
+        Product::create([
+            'nama_produk' => $request->nama_produk,
+            'harga' => $request->harga,
+            'banyak_produk' => $request->banyak_produk,
+            'kategori' => $request->kategori,
+            'deskripsi' => $request->deskripsi,
+            'gambar' => $gambar->hashName(),
+            'slug' => $validatedData['slug']
+        ]);
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 
     /**
@@ -51,7 +84,7 @@ class DashboardProductsController extends Controller
     public function show(Product $product)
     {
         return view('dashboard.products.show', [
-            "title" => "Produk Satuan Galea"
+            "title" => $product->nama_produk
         ]);
     }
 
@@ -63,7 +96,9 @@ class DashboardProductsController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        return view('dashboard.products.edit', [
+            "title" => $product->nama_produk
+        ]);
     }
 
     /**
@@ -75,7 +110,48 @@ class DashboardProductsController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        //validate form
+        $this->validate($request, [
+            'nama_produk'           => 'required',
+            'harga'                 => 'required',
+            'banyak_produk'         => 'required',
+            'kategori'              => 'required',
+            'deskripsi'             => 'required',
+            'gambar'                => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'slug'                   => 'required',
+        ]);
+
+        //check if image is uploaded
+        if ($request->hasFile('gambar')) {
+
+            //upload new gambar
+            $gambar = $request->file('gambar');
+            $gambar->storeAs('public/products', $gambar->hashName());
+
+            //delete old gambar
+            Storage::delete('public/products/' . $product->gambar);
+
+            //update prod$product with new gambar
+            $product->update([
+                'nama_produk'     => $request->nama_produk,
+                'harga'     => $request->harga,
+                'banyak_produk'     => $request->banyak_produk,
+                'kategori'     => $request->kategori,
+                'deskripsi'     => $request->deskripsi,
+                'gambar'     => $gambar->hashName(),
+                'slug'   => $request->slug,
+            ]);
+        } else {
+
+            //update post without gambar
+            $product->update([
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Diubah!']);
     }
 
     /**
@@ -86,6 +162,14 @@ class DashboardProductsController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        Product::destroy($product->id);
+
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Dihapus!']);
+    }
+
+    public function checkSlug(Request $request)
+    {
+        $slug = SlugService::createSlug(Product::class, 'slug', $request->nama_produk);
+        return response()->json(['slug' => $slug]);
     }
 }
